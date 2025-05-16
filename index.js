7966f6468069abb2d12588e04e69c54cd0078993
emanejia <emanmejia@gmail.com>
2025-05-16 15:09:15 -0700

    Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Load packages and access services
+const express = require("express");
+const app = express();
+
+// Setup view engine to ejs
+app.set('view engine', 'ejs');
+
+// Serve static content directly
+app.use(express.static("css"));
+
+// Start listening to incoming requests
+// If process.env.PORT is not defined, port number 3000 is used
+const listener = app.listen(process.env.PORT || 5001, () => {
+    console.log(`Your app is listening on port ${listener.address().port}`);
+});
+
+// Setup routes
+
+// Route to the welcome page
+app.get('/', (request, response) => {
+    response.render("index");
+});
+
+// Route to the services page
+app.get('/services', (request, response) => {
+    //Data
+    const name = "Amy";
+    response.render("services", {name: name});
+});
